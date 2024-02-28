7bb6b648764fcbc9708a8e31e14b015449572166
Julio Lobo <juliolobo@lobohost.com.br>
2024-02-28 04:37:43 -0300

    Ativando a Rota principal

--- application/config/routes.php
+++ application/config/routes.php
@@ -45,7 +45,7 @@ $route['404_override'] = '';
 
 // Rotas API
 $route['api/login']           = 'api/ApiController/login';
-// $route['api']                 = 'api/ApiController/index';
+$route['api']                 = 'api/ApiController/index';
 $route['api/clientes']        = 'api/ClientesController/index';
 $route['api/clientes/(:any)'] = 'api/ClientesController/index/$1';
 $route['api/produtos']        = 'api/ProdutosController/index';
